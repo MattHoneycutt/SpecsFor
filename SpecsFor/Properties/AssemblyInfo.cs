e10d319db803f096e5af86416c457701b7457373
matthoneycutt <mbhoneycutt@gmail.com>
2013-03-07 23:56:48 -0600

    Added a method I've been using on several projects to simplify mocking an IQueryable.
    This commit corresponds to SpecsFor V3 final.

--- SpecsFor/Properties/AssemblyInfo.cs
+++ SpecsFor/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
 [assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-RC1")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
 [assembly: InternalsVisibleTo("SpecsFor.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
