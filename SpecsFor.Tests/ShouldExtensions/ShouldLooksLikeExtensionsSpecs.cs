fc1dfabce765d3cddbc6553f11afd6a69e9b7b52
Robert Simmons <robert.simmons.tn@gmail.com>
2014-07-11 20:45:42 -0500

    Adding the actual files, that's pretty helpful I think.

---
+++ SpecsFor.Tests/ShouldExtensions/ShouldLooksLikeExtensionsSpecs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using Should.Core.Exceptions;
+using SpecsFor.ShouldExtensions;
+
+namespace SpecsFor.Tests.ShouldExtensions
+{
+	public class ShouldLooksLikeExtensionsSpecs : SpecsFor<ShouldLooksLikeExtensionsSpecs.TestObject>
+	{
+		public class TestObject
+		{
+			public Guid TestObjectId { get; set; }
+			public int Awesomeness { get; set; }
+			public string Name { get; set; }
+		}
+
+		protected override void InitializeClassUnderTest()
+		{
+			SUT = new TestObject { TestObjectId = Guid.NewGuid(), Name = "Test", Awesomeness = 11};
+		}
+
+		[Test]
+		public void then_it_should_only_check_specified_properties_good()
+		{
+			Assert.DoesNotThrow(() => SUT.ShouldLookLike(() => new TestObject
+			{
+				Name = "Test",
+				Awesomeness = 11
+			}));
+		}
+
+		[Test]
+		public void then_it_should_like_guids_too()
+		{
+			Assert.DoesNotThrow(() => SUT.ShouldLookLike(() => new TestObject
+			{
+				TestObjectId = SUT.TestObjectId
+			}));
+		}
+
+		[Test]
+		public void then_it_should_only_check_specified_properties_bad()
+		{
+			Assert.Throws<EqualException>(() => SUT.ShouldLookLike(() => new TestObject
+			{
+				Name = "Tests"
+			}));
+		}
+		
+		//TODO: Handle nested properties, arrays, etc
+	}
+}
---
+++ SpecsFor/ShouldExtensions/ShouldLooksLikeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
+using Should;
+
+namespace SpecsFor.ShouldExtensions
+{
+	public static class ShouldLooksLikeExtensions
+	{
+		public static void ShouldLookLike<T>(this T actual, Expression<Func<T>> matchFunc) where T : class
+		{
+			var memberInitExpression = (matchFunc.Body as MemberInitExpression);
+
+			if (matchFunc.Body.NodeType != ExpressionType.MemberInit || memberInitExpression == null)
+			{
+				Assert.Fail("The matching expression can only be a new object declaration.");
+			}
+
+			var expected = matchFunc.Compile()();
+
+			foreach (var memberBinding in memberInitExpression.Bindings)
+			{
+				var actualValue = typeof(T).GetProperty(memberBinding.Member.Name).GetValue(actual, null);
+				var expectedValue = typeof(T).GetProperty(memberBinding.Member.Name).GetValue(expected, null);
+
+				actualValue.ShouldEqual(expectedValue);
+			}
+		}
+	}
+}
