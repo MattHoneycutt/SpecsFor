077caa8ef564c8000c54ae2361b0fec9e66316f0
Matt Honeycutt <mbhoneycutt@gmail.com>
2014-07-24 16:57:19 -0500

    First pass at test helpers.  Still have a lot left to do!

---
+++ SpecsFor.Helpers.Web/Mvc/RouteTestingHelpers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Should;
+using Should.Core.Exceptions;
+
+namespace SpecsFor.Helpers.Web.Mvc
+{
+	internal static class RouteTestingHelpers
+	{
+		private static object GetValue(this RouteValueDictionary routeValues, string key)
+		{
+			return (from routeValueKey in routeValues.Keys 
+				where string.Equals(routeValueKey, key, StringComparison.InvariantCultureIgnoreCase) 
+				select routeValues[routeValueKey] as string).FirstOrDefault();
+		}
+
+		public static void ShouldMapTo<TController>(this RouteData routeData, Expression<Action<TController>> action)
+			where TController : Controller
+		{
+			//check controller
+			var expected = typeof(TController).Name.Replace("Controller", "");
+			var actual = (string)routeData.Values.GetValue("controller");
+			expected.ShouldEqual(actual, StringComparer.InvariantCultureIgnoreCase);
+
+			//check action
+			var methodCall = (MethodCallExpression)action.Body;
+			var expectedAction = methodCall.Method.Name;
+			var actualAction = routeData.Values.GetValue("action").ToString();
+			expectedAction.ShouldEqual(actualAction, StringComparer.InvariantCultureIgnoreCase);
+
+			//check parameters
+			for (var i = 0; i < methodCall.Arguments.Count; i++)
+			{
+				var name = methodCall.Method.GetParameters()[i].Name;
+				object value = null;
+
+				switch (methodCall.Arguments[i].NodeType)
+				{
+					case ExpressionType.Constant:
+						value = ((ConstantExpression)methodCall.Arguments[i]).Value;
+						break;
+
+					case ExpressionType.MemberAccess:
+						value = Expression.Lambda(methodCall.Arguments[i]).Compile().DynamicInvoke();
+						break;
+
+				}
+
+				value = (value == null ? null : value.ToString());
+
+				var routeValue = routeData.Values.GetValue(name);
+
+				if (!Equals(value, routeValue))
+				{
+					var message = string.Format("Route parameter '{0}' does not match. Expected '{1}', found '{2}'", name, value, routeValue);
+					throw new AssertException(message);
+				}
+			}
+		}
+	}
+}
--- SpecsFor.Helpers.Web/Mvc/ShouldActionResultExtensions.cs
+++ SpecsFor.Helpers.Web/Mvc/ShouldActionResultExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Web.Mvc;
+using System.Web.Routing;
+using Should;
+using SpecsFor.ShouldExtensions;
 
 namespace SpecsFor.Helpers.Web.Mvc
 {
@@ -7,54 +11,71 @@ namespace SpecsFor.Helpers.Web.Mvc
 	{
 		public static ViewResult ShouldRenderView(this ActionResult result)
 		{
-			throw new Exception();
+			return result.ShouldBeType<ViewResult>();
 		}
 
 		public static PartialViewResult ShouldRenderPartialView(this ActionResult result)
 		{
-			throw new Exception();
+			return result.ShouldBeType<PartialViewResult>();
 		}
 
 		public static ViewResult ShouldRenderDefaultView(this ActionResult result)
 		{
-			throw new Exception();
+			var viewResult = result.ShouldBeType<ViewResult>();
+			viewResult.ViewName.ShouldBeEmpty();
+			return viewResult;
 		}
 
-		public static ViewResult ShouldRenderDefaultPartialView(this ActionResult result)
+		public static PartialViewResult ShouldRenderDefaultPartialView(this ActionResult result)
 		{
-			throw new Exception();
+			var viewResult = result.ShouldBeType<PartialViewResult>();
+			viewResult.ViewName.ShouldBeNull();
+			return viewResult;
 		}
 
 		public static TModel WithModelType<TModel>(this ViewResult result)
 		{
-			throw new Exception();
+			return result.Model.ShouldBeType<TModel>();
+		}
+
+		public static void WithModelLike<TModel>(this ViewResult result, TModel model)
+		{
+			result.Model.ShouldBeType<TModel>()
+				.ShouldLookLike(model);
 		}
 
 		public static JsonResult ShouldReturnJson(this ActionResult result)
 		{
-			throw new Exception();
+			return result.ShouldBeType<JsonResult>();
 		}
 
 		public static TModel WithModelType<TModel>(this JsonResult result)
 		{
-			throw new Exception();
+			return result.Data.ShouldBeType<TModel>();
 		}
 
-		//TODO: Do we need to split this?  One that returns a RedirectToRouteResult, and one
-		//		that returns a more generic RedirectResult?
-		public static RedirectResult ShouldRedirect(this ActionResult result)
+		public static void ShouldRedirectTo<TController>(this ActionResult result, Expression<Action<TController>> action) where TController : Controller
 		{
-			throw new Exception();
+			var routeResult = result.ShouldBeType<RedirectToRouteResult>();
+
+			var routeData = new RouteData();
+			
+			foreach (var kvp in routeResult.RouteValues)
+			{
+				routeData.Values.Add(kvp.Key, kvp.Value);
+			}
+
+			routeData.ShouldMapTo(action);
 		}
 
-		//Tests for HTTP  status results
+		//TODO: Tests for HTTP  status results
 
-		//Tests for actions that return a model error.  Probably would be better to return a test helper
+		//TODO: Tests for actions that return a model error.  Probably would be better to return a test helper
 		//object that we can chain asserts off of so that we don't lose the model type stuff.
 
 	}
 
-	//Need some way to initialize a controller with standard/typical mocks
+	//TODO: Need some way to initialize a controller with standard/typical mocks
 	
 	//Need some way to apply model state error to controller:
 	//SUT.AddModelErrorFor<SomeForm>(x => x.Name, optionalMessage: "Required!")
