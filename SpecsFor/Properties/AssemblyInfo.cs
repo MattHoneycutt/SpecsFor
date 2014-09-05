84e2968b2fd501d74f40484bd888ae8e65cba5ef
Matt Honeycutt <mbhoneycutt@gmail.com>
2014-09-05 14:29:10 -0500

    Final bump for SpecsFor 4.1 release.

--- SpecsFor/Properties/AssemblyInfo.cs
+++ SpecsFor/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.1.0")]
 [assembly: AssemblyFileVersion("4.1.0")]
-[assembly: AssemblyInformationalVersion("4.1.0-rc07")]
+[assembly: AssemblyInformationalVersion("4.1.0")]
 [assembly: InternalsVisibleTo("SpecsFor.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
