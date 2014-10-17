7124aa0c379a6a5c8713ef04820b208e346e7bd6
Matt Honeycutt <mbhoneycutt@gmail.com>
2014-10-17 13:15:08 -0500

    Bumped NuGet package version.

--- SpecsFor.Mvc/Properties/AssemblyInfo.cs
+++ SpecsFor.Mvc/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.3.0")]
 [assembly: AssemblyFileVersion("3.3.0")]
-[assembly: AssemblyInformationalVersion("3.3.0-alpha03")]
+[assembly: AssemblyInformationalVersion("3.3.0-alpha08")]
