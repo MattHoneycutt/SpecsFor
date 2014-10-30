f2d1c9c8ae7f83e2196198b1f765bcb52ad5bd30
Matt Honeycutt <mbhoneycutt@gmail.com>
2014-10-29 21:07:42 -0500

    Bumped version number for first RC release of 4.0 of SpecsFor.Mvc.

--- SpecsFor.Mvc/Properties/AssemblyInfo.cs
+++ SpecsFor.Mvc/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0")]
-[assembly: AssemblyFileVersion("3.3.0")]
-[assembly: AssemblyInformationalVersion("3.3.0-alpha08")]
+[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.0.0")]
+[assembly: AssemblyInformationalVersion("4.0.0-rc01")]
