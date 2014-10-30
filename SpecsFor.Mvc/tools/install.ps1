39b5a616d8d5fdacefc8240d6ef619304c26b5d4
Matt Honeycutt <mbhoneycutt@gmail.com>
2014-10-29 21:06:27 -0500

    The SpecsFor.Mvc installation now checks for the required version of Microsoft Build Tools and displays an installation confirmation page.  If MS Build Tools is missing, the confirmation page includes instructions on correcting the issue.

--- SpecsFor.Mvc/tools/install.ps1
+++ SpecsFor.Mvc/tools/install.ps1
@@ -1,1 +1,14 @@
 param($installPath, $toolsPath, $package, $project)
+
+
+$MSBuildToolsPathx64 = "${env:ProgramFiles(x86)}\MSBuild\12.0\bin\msbuild.exe"
+$MSBuildToolsPathx86 = "${env:ProgramFiles}\MSBuild\12.0\bin\msbuild.exe"
+
+$MissingDependencies = (-not ((Test-Path $MSBuildToolsPathx64) -or (Test-Path $MSBuildToolsPathx86)))
+$Url = "http://specsfor.com/SpecsForMvc/installed.cshtml?missingBuildTools=" 
+
+if ($MissingDependencies) {
+	$Url = $Url + "true";
+} 
+$DTE.ItemOperations.Navigate($Url)
+#Write-Host "Target URL: $Url"
