19071d470b8aec6d9fc0dc3d5462365ed6016de5
Matt Honeycutt <mbhoneycutt@gmail.com>
2015-01-09 08:33:04 -0600

    Bumped version of SpecsFor<Web> Helpers for final 1.3.0 release.

--- SpecsFor.Helpers.Web/Properties/AssemblyInfo.cs
+++ SpecsFor.Helpers.Web/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0")]
 [assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0-alpha01")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
