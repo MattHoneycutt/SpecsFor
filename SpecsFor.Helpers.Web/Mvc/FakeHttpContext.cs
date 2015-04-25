145239ec98da8ac689507925ec121258a631cf47
Matt Honeycutt <mbhoneycutt@gmail.com>
2015-04-25 16:39:12 -0500

    Cleaned up FakeHttpContext a bit, added support for HttpContext.Items.

--- SpecsFor.Helpers.Web/Mvc/FakeHttpContext.cs
+++ SpecsFor.Helpers.Web/Mvc/FakeHttpContext.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Specialized;
-using System.Linq.Expressions;
-using System.Runtime.Remoting.Contexts;
+using System.Collections;
 using System.Security.Principal;
 using System.Web;
 using System.Web.SessionState;
@@ -11,47 +9,33 @@ namespace SpecsFor.Helpers.Web.Mvc
 {
 	public class FakeHttpContext : HttpContextBase
 	{
-		private readonly IFormParamsProvider _formParams;
-		private readonly IQueryStringParamsProvider _queryStringParams;
-		private readonly HttpCookieCollection _cookies;
 		private readonly SessionStateItemCollection _sessionItems;
-		private FakeHttpRequest _request;
-		private HttpServerUtilityBase _server;
-		private HttpResponseBase _response;
+		private readonly FakeHttpRequest _request;
+		private readonly HttpServerUtilityBase _server;
+		private readonly HttpResponseBase _response;
 		private readonly IHttpContextBehavior _behavior;
+		private readonly IDictionary _items;
 
 		public override HttpRequestBase Request
 		{
-			get
-			{
-				return (HttpRequestBase)this._request;
-			}
+			get { return _request; }
 		}
 
 		public override HttpResponseBase Response
 		{
-			get
-			{
-				return this._response;
-			}
+			get { return _response; }
 		}
 
 		public override IPrincipal User { get; set; }
 
 		public override HttpServerUtilityBase Server
 		{
-			get
-			{
-				return this._server;
-			}
+			get { return _server; }
 		}
 
 		public override HttpSessionStateBase Session
 		{
-			get
-			{
-				return (HttpSessionStateBase)new FakeHttpSessionState(this._sessionItems);
-			}
+			get { return new FakeHttpSessionState(_sessionItems); }
 		}
 
 		public override bool IsDebuggingEnabled
@@ -59,28 +43,34 @@ namespace SpecsFor.Helpers.Web.Mvc
 			get { return _behavior.IsDebuggingEnabled; }
 		}
 
+		public override IDictionary Items
+		{
+			get { return _items; }
+		}
+
 		public FakeHttpContext(
 			IPrincipal principal,
 			IFormParamsProvider formParams,
 			IQueryStringParamsProvider queryStringParams,
-			HttpCookieCollection cookies,
+			ICookieProvider cookies,
 			SessionStateItemCollection sessionItems,
 			HttpServerUtilityBase server,
 			FakeHttpRequest request,
 			IHttpContextBehavior contextBehavior)
 		{
 			User = principal;
-			_formParams = formParams ?? new EmptyFormsParamProvider();
-			_queryStringParams = queryStringParams ?? new EmptyQueryStringParamProvider();
-			_cookies = cookies ?? new HttpCookieCollection();
 			_sessionItems = sessionItems ?? new SessionStateItemCollection();
-			_request = request ?? new FakeHttpRequest();
+			_request = request ?? new FakeHttpRequest(formParams, queryStringParams, cookies);
 			_request.SetIsAuthenticated(User.Identity != null ? User.Identity.IsAuthenticated : false);
 			_server = server ?? new Mock<HttpServerUtilityBase>().Object;
+			
 			var httpResponse = new Mock<HttpResponseBase>();
 			httpResponse.Setup(x => x.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
-			this._response = httpResponse.Object;
-			this._behavior = contextBehavior;
+			_response = httpResponse.Object;
+			
+			_behavior = contextBehavior;
+
+			_items = new Hashtable();
 		}
 
 		public FakeHttpContext(string url)
@@ -100,7 +90,7 @@ namespace SpecsFor.Helpers.Web.Mvc
 			(IPrincipal)null,
 			(IFormParamsProvider)null,
 			(IQueryStringParamsProvider)null,
-			(HttpCookieCollection)null,
+			(ICookieProvider)null,
 			(SessionStateItemCollection)null,
 			(HttpServerUtilityBase)null,
 			(FakeHttpRequest)null,
--- SpecsFor.Helpers.Web/Properties/AssemblyInfo.cs
+++ SpecsFor.Helpers.Web/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.0")]
 [assembly: AssemblyFileVersion("1.5.0")]
-[assembly: AssemblyInformationalVersion("1.5.0-alpha01")]
+[assembly: AssemblyInformationalVersion("1.5.0-alpha02")]
