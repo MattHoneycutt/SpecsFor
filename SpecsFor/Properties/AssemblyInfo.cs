dc9f1e8ea03745cd7e8fdb14e92f018113d94b3e
Matt Honeycutt <mbhoneycutt@gmail.com>
2016-01-08 13:42:59 -0600

    Added a new Should extension for making sure lists contain ascending/descending items.

--- SpecsFor/Properties/AssemblyInfo.cs
+++ SpecsFor/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.3.2")]
-[assembly: AssemblyFileVersion("4.3.2")]
-[assembly: AssemblyInformationalVersion("4.3.2")]
+[assembly: AssemblyVersion("4.4.0")]
+[assembly: AssemblyFileVersion("4.4.0")]
+[assembly: AssemblyInformationalVersion("4.4.0")]
 [assembly: InternalsVisibleTo("SpecsFor.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
