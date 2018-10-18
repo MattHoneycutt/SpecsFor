e3411d490e0f3065daf80cfd3ad28f79e02ea174
Philip Peitsch <phil.peitsch@planetinnovation.com.au>
2018-10-18 13:12:56 +0800

    Prevent reflection/null ref errors when using inherited types with ShouldLookLike
    This means comparing different levels of an object hierarchy doesn't explode messily. E.g., comparing an EntityFramework proxy to it's actual class.

--- SpecsFor.Core/ShouldExtensions/ShouldLooksLikeExtensions.cs
+++ SpecsFor.Core/ShouldExtensions/ShouldLooksLikeExtensions.cs
@@ -95,12 +95,24 @@ namespace SpecsFor.Core.ShouldExtensions
 		private static void ShouldMatch(object actual, MemberInitExpression expression)
 		{
 			var expected = Expression.Lambda<Func<object>>(expression).Compile()();
-			var type = actual.GetType();
+			var expectedType = expected.GetType();
+			var actualType = actual.GetType();
 
 			foreach (var memberBinding in expression.Bindings)
 			{
-				var actualValue = type.GetProperty(memberBinding.Member.Name).GetValue(actual, null);
-				var expectedValue = type.GetProperty(memberBinding.Member.Name).GetValue(expected, null);
+				var actualProperty = actualType.GetProperty(memberBinding.Member.Name);
+				if (actualProperty == null)
+				{
+					Assert.Fail($"Unable to find property '{memberBinding.Member.Name}' on actual object of type {actualType.FullName}");
+				}
+				var actualValue = actualProperty.GetValue(actual, null);
+
+				var expectedProperty = expectedType.GetProperty(memberBinding.Member.Name);
+				if (expectedProperty == null)
+				{
+					Assert.Fail($"Unable to find property '{memberBinding.Member.Name}' on expected object of type {expectedType.FullName}");
+				}
+				var expectedValue = expectedProperty.GetValue(expected, null);
 
 				var bindingAsAnotherExpression = memberBinding as MemberAssignment;
 
