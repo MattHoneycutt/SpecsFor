3c928f4c90771664325f470942a1a93bbde1cdc1
Nelson <nelsonminayaperez@gmail.com>
2025-03-25 14:37:28 -0400

    Add SpecsForEngineSpecs

--- SpecsFor.Core/Validation/ISpecValidator.cs
+++ SpecsFor.Core/Validation/ISpecValidator.cs
@@ -3,6 +3,7 @@
 [assembly: InternalsVisibleTo("SpecsFor.Tests")]
 [assembly: InternalsVisibleTo("SpecsFor.Autofac.Tests")]
 [assembly: InternalsVisibleTo("SpecsFor.StructureMap.Tests")]
+[assembly: InternalsVisibleTo("SpecsFor.Lamar.Tests")]
 namespace SpecsFor.Core.Validation
 {
 	internal interface ISpecValidator
