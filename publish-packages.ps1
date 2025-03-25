c415222ada176e8dd0a26e3e34734b771c476357
matthoneycutt <matt@smartmoving.com>
2025-03-25 15:45:10 -0500

    Updated publishing logic, pushed new versions of everything.

--- publish-packages.ps1
+++ publish-packages.ps1
@@ -1,11 +1,14 @@
-if (Test-path .\SpecsFor.Core\bin\Debug\SpecsFor.Core.*.nupkg) {
-	rm (Resolve-Path .\SpecsFor.Core\bin\Debug\SpecsFor.Core.*.nupkg)
+if (Test-path .\SpecsFor.Core\bin\Release\SpecsFor.Core.*.nupkg) {
+	rm (Resolve-Path .\SpecsFor.Core\bin\Release\SpecsFor.Core.*.nupkg)
 }
-if (Test-path .\SpecsFor.StructureMap\bin\Debug\SpecsFor.StructureMap.*.nupkg) {
-	rm (Resolve-Path .\SpecsFor.StructureMap\bin\Debug\SpecsFor.StructureMap.*.nupkg)
+if (Test-path .\SpecsFor.StructureMap\bin\Release\SpecsFor.StructureMap.*.nupkg) {
+	rm (Resolve-Path .\SpecsFor.StructureMap\bin\Release\SpecsFor.StructureMap.*.nupkg)
 }
-if (Test-path .\SpecsFor.Autofac\bin\Debug\SpecsFor.Autofac.*.nupkg) {
-	rm (Resolve-Path .\SpecsFor.Autofac\bin\Debug\SpecsFor.Autofac.*.nupkg)
+if (Test-path .\SpecsFor.Autofac\bin\Release\SpecsFor.Autofac.*.nupkg) {
+	rm (Resolve-Path .\SpecsFor.Autofac\bin\Release\SpecsFor.Autofac.*.nupkg)
+}
+if (Test-path .\SpecsFor.Lamar\bin\Release\SpecsFor.Lamar.*.nupkg) {
+	rm (Resolve-Path .\SpecsFor.Lamar\bin\Release\SpecsFor.Lamar.*.nupkg)
 }
 if (Test-path .\SpecsFor.*.nupkg) {
 	rm (Resolve-Path .\SpecsFor.*.nupkg)
@@ -13,13 +16,15 @@ if (Test-path .\SpecsFor.*.nupkg) {
 dotnet pack .\SpecsFor.Core\SpecsFor.Core.csproj
 dotnet pack .\SpecsFor.StructureMap\SpecsFor.StructureMap.csproj
 dotnet pack .\SpecsFor.Autofac\SpecsFor.Autofac.csproj
+dotnet pack .\SpecsFor.Lamar\SpecsFor.Lamar.csproj
 nuget pack .\metapackage\specsfor-metapackage.nuspec
 
 Write-Host "Packages built, publish?"
 pause
 
-dotnet nuget push  (Resolve-Path .\SpecsFor.Core\bin\Debug\SpecsFor.Core.*.nupkg)
-dotnet nuget push  (Resolve-Path .\SpecsFor.StructureMap\bin\Debug\SpecsFor.StructureMap.*.nupkg)
-dotnet nuget push  (Resolve-Path .\SpecsFor.Autofac\bin\Debug\SpecsFor.Autofac.*.nupkg)
-dotnet nuget push  (Resolve-Path .\SpecsFor.*.nupkg)
+nuget push (Resolve-Path .\SpecsFor.Core\bin\Release\SpecsFor.Core.*.nupkg) -Source https://api.nuget.org/v3/index.json
+nuget push (Resolve-Path .\SpecsFor.StructureMap\bin\Release\SpecsFor.StructureMap.*.nupkg) -Source https://api.nuget.org/v3/index.json
+nuget push (Resolve-Path .\SpecsFor.Autofac\bin\Release\SpecsFor.Autofac.*.nupkg) -Source https://api.nuget.org/v3/index.json
+nuget push (Resolve-Path .\SpecsFor.Lamar\bin\Release\SpecsFor.Lamar.*.nupkg) -Source https://api.nuget.org/v3/index.json
+nuget push (Resolve-Path .\SpecsFor.*.nupkg) -Source https://api.nuget.org/v3/index.json
 
